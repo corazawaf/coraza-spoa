57efefa1a3f0e9086ac00198849d97bad5c1b99a
Thomas Anderson <127358482+zc-devs@users.noreply.github.com>
2023-06-21 14:11:16 +0300

    Different exceptions on message parsing

--- internal/message.go
+++ internal/message.go
@@ -8,50 +8,80 @@ import (
 	"net"
 )
 
+type keyNotFoundError struct {
+	key string
+}
+
+func (e *keyNotFoundError) Error() string {
+	return fmt.Sprintf("Key '%s' not found", e.key)
+}
+
+type typeMismatchError struct {
+	key          string
+	expectedType string
+	actualValue  interface{}
+}
+
+func (e *typeMismatchError) Error() string {
+	return fmt.Sprintf("Invalid argument for %s, %s expected, got %T", e.key, e.expectedType, e.actualValue)
+}
+
 func getString(args map[string]interface{}, key string) (string, error) {
 	argVal, exist := args[key]
-	if exist && argVal != nil {
-		val, ok := argVal.(string)
-		if ok {
-			return val, nil
-		}
+	if !exist {
+		return "", &keyNotFoundError{key}
 	}
-
-	return "", fmt.Errorf("invalid argument for %s, string expected, got %v", key, argVal)
+	if argVal == nil {
+		return "", nil
+	}
+	val, ok := argVal.(string)
+	if !ok {
+		return "", &typeMismatchError{key, "string", argVal}
+	}
+	return val, nil
 }
 
 func getInt(args map[string]interface{}, key string) (int, error) {
 	argVal, exist := args[key]
-	if exist && argVal != nil {
-		val, ok := argVal.(int)
-		if ok {
-			return val, nil
-		}
+	if !exist {
+		return 0, &keyNotFoundError{key}
 	}
-
-	return 0, fmt.Errorf("invalid argument for %s, integer expected, got %v", key, argVal)
+	if argVal == nil {
+		return 0, nil
+	}
+	val, ok := argVal.(int)
+	if !ok {
+		return 0, &typeMismatchError{key, "int", argVal}
+	}
+	return val, nil
 }
 
 func getByteArray(args map[string]interface{}, key string) ([]byte, error) {
 	argVal, exist := args[key]
-	if exist && argVal != nil {
-		val, ok := argVal.([]byte)
-		if ok {
-			return val, nil
-		}
+	if !exist {
+		return nil, &keyNotFoundError{key}
 	}
-
-	return nil, fmt.Errorf("invalid argument for %s, []byte expected, got %v", key, argVal)
+	if argVal == nil {
+		return nil, nil
+	}
+	val, ok := argVal.([]byte)
+	if !ok {
+		return nil, &typeMismatchError{key, "[]byte", argVal}
+	}
+	return val, nil
 }
 
 func getIp(args map[string]interface{}, key string) (net.IP, error) {
 	argVal, exist := args[key]
-	if exist && argVal != nil {
-		val, ok := argVal.(net.IP)
-		if ok {
-			return val, nil
-		}
+	if !exist {
+		return nil, &keyNotFoundError{key}
 	}
-
-	return nil, fmt.Errorf("invalid argument for %s, net.IP expected, got %v", key, argVal)
+	if argVal == nil {
+		return nil, nil
+	}
+	val, ok := argVal.(net.IP)
+	if !ok {
+		return nil, &typeMismatchError{key, "net.IP", argVal}
+	}
+	return val, nil
 }
--- internal/message_test.go
+++ internal/message_test.go
@@ -15,99 +15,109 @@ import (
 type testData struct {
 	key           string
 	value         interface{}
+	requestKey    string
 	expectedValue interface{}
-	expectedError string
+	expectedError reflect.Type
 }
+
+var (
+	keyNotFoundErrorType  = reflect.TypeOf((*keyNotFoundError)(nil))
+	typeMismatchErrorType = reflect.TypeOf((*typeMismatchError)(nil))
+)
 
 func TestGetString(t *testing.T) {
 	testMatrix := []testData{
-		{"non-empty", "qweasd", "qweasd", ""},
-		{"empty", "", "", ""},
-		{"null", nil, "", "invalid argument for null, string expected, got <nil>"},
+		{"non-empty", "some-value", "non-empty", "some-value", nil},
+		{"empty", "", "empty", "", nil},
+		{"null", nil, "null", "", nil},
+		{"int-type", 1, "int-type", "", typeMismatchErrorType},
+		{"map-key", "value", "other-key", "", keyNotFoundErrorType},
 	}
 
 	for _, data := range testMatrix {
 		args := map[string]interface{}{
 			data.key: data.value,
 		}
-		output, err := getString(args, data.key)
+		output, err := getString(args, data.requestKey)
 		if output != data.expectedValue {
 			t.Errorf("getString(%v, %v): output '%v' not equal to expected '%v'", args, data.key, output, data.expectedValue)
 		}
-		if errorToString(err) != data.expectedError {
-			t.Errorf("getString(%v, %v): error '%v' not equal to expected '%v'", args, data.key, err, data.expectedError)
+		errType := reflect.TypeOf(err)
+		if data.expectedError != errType {
+			t.Errorf("getString(%v, %v): error '%v' not equal to expected '%v'", args, data.key, errType, data.expectedError)
 		}
 	}
 }
 
 func TestGetInt(t *testing.T) {
 	testMatrix := []testData{
-		{"non-zero", 11, 11, ""},
-		{"zero", 0, 0, ""},
-		{"null", nil, 0, "invalid argument for null, integer expected, got <nil>"},
+		{"non-zero", 11, "non-zero", 11, nil},
+		{"zero", 0, "zero", 0, nil},
+		{"null", nil, "null", 0, nil},
+		{"string-type", "1", "string-type", 0, typeMismatchErrorType},
+		{"map-key", 11, "other-key", 0, keyNotFoundErrorType},
 	}
 
 	for _, data := range testMatrix {
 		args := map[string]interface{}{
 			data.key: data.value,
 		}
-		output, err := getInt(args, data.key)
+		output, err := getInt(args, data.requestKey)
 		if output != data.expectedValue {
 			t.Errorf("getInt(%v, %v): output '%v' not equal to expected '%v'", args, data.key, output, data.expectedValue)
 		}
-		if errorToString(err) != data.expectedError {
-			t.Errorf("getInt(%v, %v): error '%v' not equal to expected '%v'", args, data.key, err, data.expectedError)
+		errType := reflect.TypeOf(err)
+		if data.expectedError != errType {
+			t.Errorf("getString(%v, %v): error '%v' not equal to expected '%v'", args, data.key, errType, data.expectedError)
 		}
 	}
 }
 
 func TestGetByteArray(t *testing.T) {
 	testMatrix := []testData{
-		{"non-empty", []byte{99, 100}, []byte{99, 100}, ""},
-		{"empty", []byte{}, []byte{}, ""},
-		{"null", nil, nil, "invalid argument for null, []byte expected, got <nil>"},
+		{"non-empty", []byte{99, 100}, "non-empty", []byte{99, 100}, nil},
+		{"empty", []byte{}, "empty", []byte{}, nil},
+		{"null", nil, "null", nil, nil},
+		{"string-type", "str", "string-type", nil, typeMismatchErrorType},
+		{"map-key", []byte{}, "other-key", nil, keyNotFoundErrorType},
 	}
 
 	for _, data := range testMatrix {
 		args := map[string]interface{}{
 			data.key: data.value,
 		}
-		output, err := getByteArray(args, data.key)
+		output, err := getByteArray(args, data.requestKey)
 		if !(output == nil && data.expectedValue == nil || reflect.DeepEqual(output, data.expectedValue)) {
 			t.Errorf("getByteArray(%v, %v): output '%v' not equal to expected '%v'", args, data.key, output, data.expectedValue)
 		}
-		if errorToString(err) != data.expectedError {
-			t.Errorf("getByteArray(%v, %v): error '%v' not equal to expected '%v'", args, data.key, err, data.expectedError)
+		errType := reflect.TypeOf(err)
+		if data.expectedError != errType {
+			t.Errorf("getString(%v, %v): error '%v' not equal to expected '%v'", args, data.key, errType, data.expectedError)
 		}
 	}
 }
 
 func TestGetIp(t *testing.T) {
 	testMatrix := []testData{
-		{"ipv4", net.IPv4(192, 168, 1, 1), net.IPv4(192, 168, 1, 1), ""},
-		{"ipv6", net.ParseIP("::1"), net.ParseIP("::1"), ""},
-		{"invalid-ipv4", net.ParseIP("321.456.789.10"), net.ParseIP("321.456.789.10"), ""},
-		{"invalid-type", "127.0.0.1", nil, "invalid argument for invalid-type, net.IP expected, got 127.0.0.1"},
-		{"null", nil, nil, "invalid argument for null, net.IP expected, got <nil>"},
+		{"ipv4", net.IPv4(192, 168, 1, 1), "ipv4", net.IPv4(192, 168, 1, 1), nil},
+		{"ipv6", net.ParseIP("::1"), "ipv6", net.ParseIP("::1"), nil},
+		{"invalid-ipv4", net.ParseIP("321.456.789.10"), "invalid-ipv4", net.ParseIP("321.456.789.10"), nil},
+		{"null", nil, "null", nil, nil},
+		{"invalid-type", "127.0.0.1", "invalid-type", nil, typeMismatchErrorType},
+		{"map-key", net.IPv4(192, 168, 1, 1), "other-key", nil, keyNotFoundErrorType},
 	}
 
 	for _, data := range testMatrix {
 		args := map[string]interface{}{
 			data.key: data.value,
 		}
-		output, err := getIp(args, data.key)
+		output, err := getIp(args, data.requestKey)
 		if !(output == nil && data.expectedValue == nil || data.expectedValue.(net.IP).Equal(output)) {
 			t.Errorf("getIp(%v, %v): output '%v' not equal to expected '%v'", args, data.key, output, data.expectedValue)
 		}
-		if errorToString(err) != data.expectedError {
-			t.Errorf("getIp(%v, %v): error '%v' not equal to expected '%v'", args, data.key, err, data.expectedError)
+		errType := reflect.TypeOf(err)
+		if data.expectedError != errType {
+			t.Errorf("getString(%v, %v): error '%v' not equal to expected '%v'", args, data.key, errType, data.expectedError)
 		}
 	}
-}
-
-func errorToString(err error) string {
-	if err == nil {
-		return ""
-	}
-	return err.Error()
 }
