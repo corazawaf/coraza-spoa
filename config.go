dfca3ee7c45016cfdb671856b11023f97ab989a3
Rath Pascal <rath@oxl.at>
2024-12-07 12:00:22 +0100

    feat: print matched error logs as json if requested
    Previously the json handler would only wrap the message in a json object. This now replaces that message with an embedded object that contains all the information.

--- config.go
+++ config.go
@@ -163,6 +163,7 @@ func (c config) newApplications() (map[string]*internal.Application, error) {
 			Logger:         logger,
 			Directives:     a.Directives,
 			ResponseCheck:  a.ResponseCheck,
+			LogFormat:      a.Log.Format,
 			TransactionTTL: time.Duration(a.TransactionTTLMS) * time.Millisecond,
 		}
 
